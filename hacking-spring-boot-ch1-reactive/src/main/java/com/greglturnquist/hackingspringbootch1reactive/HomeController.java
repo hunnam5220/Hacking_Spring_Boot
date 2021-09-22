c882e77e95cb33ec8728a224fbbdf89604574f24
juuuun_b  <gudwns5220@gmail.com>
2021-09-22 21:56:02 +0900

    study 1

---
+++ hacking-spring-boot-ch1-reactive/src/main/java/com/greglturnquist/hackingspringbootch1reactive/HomeController.java
@@ -0,0 +1,15 @@
+package com.greglturnquist.hackingspringbootch1reactive;
+
+import reactor.core.publisher.Mono;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    Mono<String> home(){
+        return Mono.just("home");
+    }
+}
